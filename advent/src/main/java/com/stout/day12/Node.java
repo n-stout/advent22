41a3702f05166fca7a45eb5c96ccd3568328ac22
Thanacus <thanacus@gmail.com>
2022-12-14 16:15:44 -0700

    Day 12 complete.

--- advent/src/main/java/com/stout/day12/Node.java
+++ advent/src/main/java/com/stout/day12/Node.java
@@ -1,23 +1,27 @@
 package com.stout.day12;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Node {
-    public final static Set<Node> nodes = new HashSet<>();
+    public final static Map<String, Node> nodes = new HashMap<>();
 
     private Set<Node> links;
     private int row;
     private int col;
     private char height;
     private int depth;
+    private Node parent;
 
-    public Node(int row, int col, char height, int depth) {
+    public Node(int row, int col, char height, int depth, Node parent) {
         this.links = new HashSet<>();
         this.row = row;
         this.col = col;
         this.height = height;
         this.depth = depth;
+        this.parent = parent;
     }
 
     public Node getNode(String[][] field, String direction) {
@@ -31,7 +35,7 @@ public class Node {
                     int newCol = coords[1];
                     char height = field[newRow][newCol].charAt(0);
                     if(this.height >= height - 1) {
-                        return new Node(newRow, newCol, height, this.depth + 1);
+                        return new Node(newRow, newCol, height, this.depth + 1, this);
                     } else {
                         return null;
                     }
@@ -40,23 +44,52 @@ public class Node {
                 if(coords[0] == field.length - 1) {
                     return null;
                 } else {
-                    return new Node(coords[0]+1, coords[1], field[coords[0]+1][coords[1]], this.depth + 1);
+                    int newRow = coords[0]+1;
+                    int newCol = coords[1];
+                    char height = field[newRow][newCol].charAt(0);
+                    if(this.height >= height - 1) {
+                        return new Node(newRow, newCol, height, this.depth + 1, this);
+                    } else {
+                        return null;
+                    }
                 }
             case "West":
                 if(coords[1] == 0) {
                     return null;
                 } else {
-                    return new Node(coords[0], (coords[1]-1), field[coords[0]][coords[1]-1], this.depth + 1);
+                    int newRow = coords[0];
+                    int newCol = coords[1]-1;
+                    char height = field[newRow][newCol].charAt(0);
+                    if(this.height >= height - 1) {
+                        return new Node(newRow, newCol, height, this.depth + 1, this);
+                    } else {
+                        return null;
+                    }
                 }
             case "East":
                 if(coords[1] == field[0].length - 1) {
                     return null;
                 } else {
-                    return new Node(coords[0], (coords[1]+1), field[coords[0]][coords[1]+1], this.depth + 1);
+                    int newRow = coords[0];
+                    int newCol = coords[1]+1;
+                    char height = field[newRow][newCol].charAt(0);
+                    if(this.height >= height - 1) {
+                        return new Node(newRow, newCol, height, this.depth + 1, this);
+                    } else {
+                        return null;
+                    }
                 }
             default:
                 throw new RuntimeException("Boom");
         }
+    }
+
+    public void remove() {
+        this.parent.links.remove(this);
+    }
+
+    public Node getParent() {
+        return this.parent;
     }
 
     public void addLink(Node link) {
@@ -71,7 +104,7 @@ public class Node {
         return col;
     }
 
-    public String getHeight() {
+    public char getHeight() {
         return height;
     }
 
@@ -89,7 +122,7 @@ public class Node {
         int result = 1;
         result = prime * result + row;
         result = prime * result + col;
-        result = prime * result + ((height == null) ? 0 : height.hashCode());
+        result = prime * result + height;
         return result;
     }
 
@@ -106,10 +139,7 @@ public class Node {
             return false;
         if (col != other.col)
             return false;
-        if (height == null) {
-            if (other.height != null)
-                return false;
-        } else if (!height.equals(other.height))
+        if (height != other.height)
             return false;
         return true;
     }
--- advent/src/main/java/com/stout/day12/Scenario.java
+++ advent/src/main/java/com/stout/day12/Scenario.java
@@ -4,9 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scenario {
 
@@ -14,6 +13,7 @@ public class Scenario {
 
     public Scenario(String inputFile) {
         this.inputFile = new File(inputFile);
+        Node.nodes.clear();
     }
 
     public void runScenarioA() {
@@ -60,75 +60,96 @@ public class Scenario {
             }
         }
 
-        Node head = new Node(startRow, startCol, "a", 0);
-        Node.nodes.add(head);
+        Node head = new Node(startRow, startCol, 'a', 0, null);
+        Node.nodes.put(head.getKey(), head);
 
         attachNodes(field, head);
+
+        System.out.println(Node.nodes.get(endRow + "-" + endCol));
     }
 
     private void attachNodes(String[][] field, Node startingNode) {
-        Node northNode = startingNode.getNode(field, "North");
-
-        // North
-        
-    }
-
-    private int calculatePath(int steps, int startRow, int startCol, int endRow, int endCol, String[][] array, Map<String,Integer> hitPaths) {
-        if(startRow == endRow && startCol == endCol) {
-            System.out.println("Found end!");
-            return steps;
-        }
-        System.out.println("Calculating from " + startRow + "," + startCol + " to " + endRow + ", " + endCol);
-        int minSteps = Integer.MAX_VALUE;
-        if(hitPaths.containsKey(startRow + "-" + startCol) && hitPaths.get(startRow + "-" + startCol) < steps) {
-            steps = hitPaths.get(startRow + "-" + startCol);
-        } else if(hitPaths.containsKey(startRow + "-" + startCol)) {
-            return minSteps;
-        } else {
-            hitPaths.put(startRow + "-" + startCol, steps);
-        }
-        
-        // North
-        if(startRow > 0 && array[startRow-1][startCol].charAt(0) <= array[startRow][startCol].charAt(0)+1) {
-            System.out.println("Heading north to " + array[startRow-1][startCol].charAt(0));
-            minSteps = Math.min(minSteps, calculatePath(steps + 1, startRow - 1, startCol, endRow, endCol, array, hitPaths));
-        }
-
-        // South
-        if(startRow < array.length - 1 && array[startRow+1][startCol].charAt(0) <= array[startRow][startCol].charAt(0)+1) {
-            System.out.println("Heading south to " + array[startRow+1][startCol].charAt(0));
-            minSteps = Math.min(minSteps, calculatePath(steps + 1, startRow + 1, startCol, endRow, endCol, array, hitPaths));
-        }
-
-        // West
-        if(startCol > 0 && array[startRow][startCol-1].charAt(0) <= array[startRow][startCol].charAt(0)+1) {
-            
-            System.out.println("Heading west to " + array[startRow][startCol-1].charAt(0));
-            minSteps = Math.min(minSteps, calculatePath(steps + 1, startRow, startCol - 1, endRow, endCol, array, hitPaths));
-        }
-
-        // East
-        if(startCol < array[0].length - 1 && array[startRow][startCol+1].charAt(0) <= array[startRow][startCol].charAt(0)+1) {
-            System.out.println("Heading east to " + array[startRow][startCol+1].charAt(0));
-            minSteps = Math.min(minSteps, calculatePath(steps + 1, startRow, startCol + 1, endRow, endCol, array, hitPaths));
-        }
-
-        return minSteps;
+        String[] directions = new String[] { "North", "South", "East", "West" };
+        for(String direction : directions) {
+            Node node = startingNode.getNode(field, direction);
+            if(node == null) {
+                continue;
+            }
+            Node originalNode = Node.nodes.get(node.getKey());
+            if(originalNode != null) {
+                if(node.getDepth() < originalNode.getDepth()) {
+                    // New wins
+                    startingNode.addLink(node);
+                    originalNode.remove();
+                    Node.nodes.put(node.getKey(), node);
+                    attachNodes(field, node);
+                }
+            } else {
+                startingNode.addLink(node);
+                Node.nodes.put(node.getKey(), node);
+                attachNodes(field, node);
+            }
+        }        
     }
 
     public void runScenarioB() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-            String line = reader.readLine();
+        StringBuilder chars = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+			String line = reader.readLine();
+            boolean first = true;
+			while (line != null) {
+                if(!first) {
+                    chars.append(":");
+                }
+                chars.append(line);
+                first = false;
 
-            while (line != null) {
                 // Do thing
-                line = reader.readLine();
-            }
+				line = reader.readLine();
+			}
 
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        int startRow = -1;
+        int startCol = -1;
+        int endRow = -1;
+        int endCol = -1;
+        List<int[]> startingPositions = new ArrayList<>();
+
+        System.out.println(chars.toString());
+
+        String[] rows = chars.toString().split(":");
+        String[][] field = new String[rows.length][];
+        for(int i=0; i<rows.length; i++) {
+            field[i] = rows[i].split("");
+            if(rows[i].contains("a") || rows[i].contains("S")) {
+                String[] starts = rows[i].split("");
+                for(int j=0; j<starts.length; j++) {
+                    String start = starts[j];
+                    if(start.equals("a") || start.equals("S")) {
+                        startingPositions.add(new int[] { i, j });
+                    }
+                }
+            }
+            if(rows[i].contains("E")) {
+                endRow = i;
+                endCol = rows[i].indexOf("E");
+                field[endRow][endCol] = "z";
+            }
         }
+
+        int lowest = Integer.MAX_VALUE;
+        for(int[] start : startingPositions) {
+            Node head = new Node(start[0], start[1], 'a', 0, null);
+            Node.nodes.put(head.getKey(), head);
+            attachNodes(field, head);
+            lowest = Math.min(lowest, Node.nodes.get(endRow + "-" + endCol).getDepth());
+        }
+
+        System.out.println(lowest);
     }
 
     public static void main(String[] args) {
@@ -137,9 +158,9 @@ public class Scenario {
         Scenario mainA = new Scenario("inputs/day12a-input.txt");
         mainA.runScenarioA();
 
-        Scenario testB = new Scenario("inputs/day12b-test.txt");
+        Scenario testB = new Scenario("inputs/day12a-test.txt");
         testB.runScenarioB();
-        Scenario mainB = new Scenario("inputs/day12b-input.txt");
+        Scenario mainB = new Scenario("inputs/day12a-input.txt");
         mainB.runScenarioB();
 
     }
